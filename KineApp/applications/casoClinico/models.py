91000053d61b6a6e7f1cc449359c9778a2d7fbba
danielacofre <daniela.cofre@alumnos.ucn.cl>
2025-10-30 19:35:32 -0300

    corrección tabla caso

--- KineApp/applications/casoClinico/models.py
+++ KineApp/applications/casoClinico/models.py
@@ -3,10 +3,10 @@ from django.db import models
 
 # Create your models here.
 class Caso(models.Model):
-    Descripción=models.CharField('Descripción', max_length=300, null=False, default="Descripción no disponible")
+    Descripción=models.CharField('Descripción', max_length=300, null=False, default="")
    
 
     def __str__(self):
-        return str(self.id)+'-'+self.nombre+'-'+self.apellido
+        return str(self.id)+'-'+self.Descripción
     
 
