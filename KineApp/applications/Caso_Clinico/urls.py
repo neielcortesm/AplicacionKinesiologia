c56787f4e7a0716006602c9e0715a53a8d792243
neielcortesm <neiel.cortes@alumnos.ucn.cl>
2025-11-17 15:03:20 -0300

    List All Casos

--- KineApp/applications/Caso_Clinico/urls.py
+++ KineApp/applications/Caso_Clinico/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.Home.as_view(), name='home'),
+    path('list_all_casos/', views.ListAllCasos.as_view(), name='listAllCasos'),   
 ]
--- KineApp/applications/Caso_Clinico/views.py
+++ KineApp/applications/Caso_Clinico/views.py
@@ -1,5 +1,14 @@
 from django.shortcuts import render
-from django.views.generic import TemplateView
+from django.views.generic import (
+    TemplateView,
+    ListView, 
+    )
+from .models import *
 
 class Home(TemplateView):
     template_name = 'Caso_Clinico/home.html'
+
+class ListAllCasos(ListView):
+    template_name = 'Caso_Clinico/list_all_casos.html'
+    model = CasoClinico
+    context_object_name = 'ListAllCasos'
