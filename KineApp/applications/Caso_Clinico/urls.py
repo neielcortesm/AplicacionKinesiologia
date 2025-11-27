36d8bf62f1916284eedddfe24a18bea646f35fb9
neielcortesm <neiel.cortes@alumnos.ucn.cl>
2025-11-27 19:56:25 -0300

    arreglo error iniciar

--- KineApp/applications/Caso_Clinico/urls.py
+++ KineApp/applications/Caso_Clinico/urls.py
@@ -4,6 +4,7 @@ from . import views
 
 urlpatterns = [
     path('inicio/', views.Inicio.as_view(), name='Inicio'),
+    path('', views.Inicio.as_view(), name='Inicio'),
     path('list_all_casos/', views.ListAllCasos.as_view(), name='listAllCasos'),  
     path('list_casosByCategoria/', views.ListByCategoriaCasos.as_view(), name='listByCategoriaCasos'),
     path('verCaso/<pk>', views.DetailCaso.as_view(), name='detalleCaso'),
